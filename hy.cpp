cf4d6d2a2a421b2cced623843618426eb7c277d8
Yagniksiddhapura1130 <119499656+Yagniksiddhapura1130@users.noreply.github.com>
2023-02-14 16:49:27 +0530

    Add files via upload

--- hy.cpp
+++ hy.cpp
@@ -18,7 +18,7 @@ class info
 //		}
 		void displayinfo()
 		{	
-			int ordr;
+			int ordr,ordrs;
 		
 			cout<<"\t\t\t\t-------------tops tech food ------------"<<endl<<endl;
 			
@@ -34,44 +34,101 @@ class info
 			cout<<"3) rolls"<<endl;	
 			cout<<"4) biryani"<<endl;
 			
-            cout<<"please enter your choice : ";
-            cin>>ordr;
+            cout<<endl<<"please enter your choice : ";
+            cin>>ordrs;
 
 
 			}
 
         void ordrdetails()
         {
-            int ordr;
+            int ordr,ordrs;
            
         switch(ordr)
         { case 1:
-            cout<<endl<<"double cheese margarita = 250"<<endl;
-            cout<<"cheese corn=200"<<endl;
-            cout<<"veg pizza=280"<<endl;
+            cout<<endl<<"1)double cheese margarita = 250"<<endl;
+            cout<<"2)cheese corn=200"<<endl;
+            cout<<"3)veg pizza=280"<<endl;
 
+            cout<<endl<<"please enter which pizza would you like to have : ";
+            cin>>ordrs;
             break;
 
             case 2:
-            cout<<endl<<"Aloo Grilled Sandwich=70"<<endl;
-            cout<<"Mixed Vegetable Sandwich=100"<<endl;
-            cout<<"Corn Cheese Sandwich=60"<<endl;
-            cout<<"veg sandwitch=80"<<endl;
+            cout<<endl<<"1)Aloo Grilled Sandwich=70"<<endl;
+            cout<<"2)Mixed Vegetable Sandwich=100"<<endl;
+            cout<<"3)Corn Cheese Sandwich=60"<<endl;
+            cout<<"4)veg sandwitch=80"<<endl;
+
+            break;
+            
+            case 3:
+            cout<<endl<<"1)Stuffed Moong Dal and Potato Rolls = 70"<<endl;
+            cout<<"2)Aloo Corn Tikki Roll=80"<<endl;
+            cout<<"3)Mexican Bread Roll =100"<<endl;
+            cout<<"4)Potato Cream Cheese Roll=50"<<endl;
+            cout<<"5)chineese roll"<<endl;
+
+            break;
+
+            case 4:
+            cout<<endl<<"1)hydrabadi biryani=200"<<endl;
+            cout<<"2)kashmiri biryani=120"<<endl;
+            cout<<"3)veg biryani=150"<<endl;
+            cout<<"4)handi biryani=100"<<endl;       
 
             break;
         }
-
+        
+        }
         void ordrprize()
 
-        { int  quantity,orderprice=0;
+    { 
+        int ordrs;
+        int  quan,orderprice=0,totalprice;
+          switch (ordrs)
+    
+        {            
+         
 
+            case 1:
+            { 
+            
         
+            cout<<"enter quantity : ";
+            cin>>quan;
 
-        }
-
+            
+			orderprice=250; 
+            totalprice = orderprice*quan; 
+            cout<<endl<<"Your Order Price is Rs="<<orderprice<<endl<<"Your Total Price is Rs= "<<totalprice; 
+            
+            break; 
         
+            case 2:
+            cout<<"enter quantity : ";
+            cin>>quan;
 
+            
+			orderprice=200; 
+            totalprice = orderprice*quan; 
+            cout<<endl<<"Your Order Price is Rs="<<orderprice<<endl<<"Your Total Price is Rs= "<<totalprice; 
+            
+            break; 
+
+            cout<<"enter quantity : ";
+            cin>>quan;
+
+            
+			orderprice=280; 
+            totalprice = orderprice*quan; 
+            cout<<endl<<"Your Order Price is Rs="<<orderprice<<endl<<"Your Total Price is Rs= "<<totalprice; 
+            
+            break;   
         }
+        
+        }
+    }
 };
 
 int main()
@@ -80,6 +137,6 @@ int main()
 //	i.getinfo();//
 	i.displayinfo();
     i.ordrdetails();
-
+    i.ordrprize();
 	return 0;
 }
